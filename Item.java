682ae02082b9ac1dc4d964372312aa31f861f9fd
discordgr <Razer_greece@yahoo.gr>
2016-04-14 16:45:40 +0300

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,47 @@
+public class Item {
+
+   	public String title;
+   	public int playingTime;
+   	public float price;
+
+    public Item() 
+	{
+		
+	}
+
+    public Item(String theTitle, int time, float price) 
+	{
+    	this.title = theTitle;
+		this.playingTime = time;
+		this.price = price;
+    }
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+    public String getTitle() {
+        return title;
+    }
+
+   	public int getplayingTime() {
+        return playingTime;
+    }
+
+   	public Float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Float price){
+         this.price=price;
+    }
+
+   	public void setplayingTime(int  playingTime) {
+         this.playingTime=playingTime;
+    }
+
+    public void print() {
+        System.out.println("title: "+getTitle() + " (" + getplayingTime() + " mins)");
+        System.out.println("Price: "+getPrice());
+    }
+}
